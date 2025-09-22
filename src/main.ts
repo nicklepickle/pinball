6c4e8bd48ecd7f57b5f7a228fe24e1826d61d753
NicklePickle <nick@imac.mynetworksettings.com>
2025-09-22 07:15:50 -0400

    Added project to git

---
+++ src/main.ts
@@ -0,0 +1,170 @@
+import decomp from 'poly-decomp';
+import { Engine, Render, Runner, Bodies, Composite, Common, Body, Events, Collision, Vector} from 'matter-js';
+
+let forces: Matter.Vector[] = []; // forces that need to be applied
+function launchBall(ball: Body, from: Vector, velocity: number) {
+    var deltaX = from.x - ball.position.x;
+    var deltaY = from.y - ball.position.y;
+    var a = Math.atan2(deltaY, deltaX);
+
+    var x = velocity * -Math.cos(a);
+    var y = velocity * -Math.sin(a);
+    forces.push( {x:x, y:y}); // can't apply force in an event handler (dumb)
+}
+
+function getArchVerts(innerRadius: number, outerRadius: number): Matter.Vector[][]  {
+    var archVerts = []
+    for(var a=0; a<Math.PI; a+=.2) {
+      var x = -Math.round(outerRadius*Math.cos(a))
+      var y = -Math.round(outerRadius*Math.sin(a))
+      archVerts.push({x:x,y:y})
+    }
+
+    for(var a=Math.PI; a>0; a-=.2) {
+      var x = -Math.round(innerRadius*Math.cos(a))
+      var y = -Math.round(innerRadius*Math.sin(a))
+      archVerts.push({x:x,y:y})
+    }
+
+    return [archVerts];
+}
+
+Common.setDecomp(decomp) // use poly-decomp for concave bodies
+
+window.addEventListener('load', () => {
+
+const $canvas: HTMLCanvasElement = document.getElementById('canvas') as HTMLCanvasElement; 
+
+var engine: Engine = Engine.create();
+engine.gravity = {x:0,y:.00038,scale:1}
+var render: Render = Render.create({
+    canvas: $canvas,
+    engine: engine,
+});
+
+// set the canvas size AFTER createding Render
+$canvas.width = 600;
+$canvas.height = 900;
+
+/*---------------- move to scene function -------------- */
+
+var leftBlock = Bodies.trapezoid(24,420,140,30,Math.PI/5, { isStatic: true, angle: Math.PI/2 });
+var rightBlock = Bodies.trapezoid(520,420,140,30,Math.PI/5, { isStatic: true, angle: -Math.PI/2 });
+
+var leftLane = Bodies.fromVertices(80,700,[[{x:0,y:0},{x:0,y:110},{x:90,y:130}]], {isStatic: true})
+var rightLane = Bodies.fromVertices(460,700,[[{x:0,y:0},{x:0,y:110},{x:-90,y:130}]], {isStatic: true})
+
+var leftEdge: Body  = Bodies.rectangle(0, 550, 20, 700, { isStatic: true });
+var rightEdge: Body  = Bodies.rectangle(600, 550, 20, 700, { isStatic: true });
+var laneBoundry: Body  = Bodies.rectangle(543, 600, 20, 600, { isStatic: true });
+
+var spring: Body  = Bodies.rectangle(572, 860, 30, 30, { isStatic: true });
+
+var leftRamp: Body  = Bodies.rectangle(90, 800, 160, 20, { isStatic: true,  angle: Math.PI * .07});
+var rightRamp: Body  = Bodies.rectangle(470, 800, 160, 20, { isStatic: true,  angle: Math.PI * -.07 });
+
+var arch = Bodies.fromVertices(320,100,getArchVerts(290,360), { isStatic: true });
+var oob = Bodies.rectangle(300,910,600,10, { isStatic: true, isSensor:true })
+
+var leftFlipper: Body = Bodies.fromVertices(200, 824, [[{x:0,y:0},{x:80, y:20},{x:80,y:24},{x:0,y:24}]],{isStatic:true})
+var rightFlipper: Body = Bodies.fromVertices(360, 824, [[{x:0,y:0},{x:-80, y:20},{x:-80,y:24},{x:0,y:24}]],{isStatic:true,})
+
+var bouncer1: Matter.Body = Bodies.circle(300, 330, 40, {isStatic: true, label:'bouncer'  })
+var bouncer2: Matter.Body = Bodies.circle(220, 200, 40, {isStatic: true, label:'bouncer'  })
+var bouncer3: Matter.Body = Bodies.circle(380, 200, 40, {isStatic: true, label:'bouncer'  })
+
+var ball: Matter.Body = Bodies.circle(572, 700, 15, {restitution:.3  })
+
+/* ----------------------------------------------------- */
+
+// add all of the bodies to the world
+Composite.add(engine.world, [spring, leftRamp, rightRamp, arch, leftFlipper, 
+    rightFlipper,leftBlock,rightBlock,leftLane,rightLane,
+  leftEdge, rightEdge, laneBoundry, oob, bouncer1, bouncer2, bouncer3, ball]);
+
+// run the renderer
+Render.run(render);
+
+// create runner
+var runner: Runner = Runner.create();
+
+var downTime: Date;
+var onSpring = false;
+
+$canvas.addEventListener('mousedown', () => {
+    if (onSpring) {
+        downTime = new Date();
+    }
+    else {
+        if (ball.position.x < 550) {
+            Body.rotate(leftFlipper, Math.PI * -.15);
+            Body.rotate(rightFlipper, Math.PI * .15);
+        }
+        if (Collision.collides(leftFlipper, ball)) {
+           launchBall(ball,leftFlipper.position,.06)
+        }
+        else if (Collision.collides(rightFlipper, ball)) {
+           launchBall(ball,rightFlipper.position,.06)
+        }
+    }
+})
+
+$canvas.addEventListener('mouseup', () => {
+    if (onSpring) {
+        var x = Math.random() * .01 - .005;
+        var y = -Math.min((new Date().getTime()- downTime.getTime()) / 5000, .1);
+        var force: Matter.Vector = {x:x, y:y};
+        Body.applyForce(ball,ball.position,force)
+        onSpring = false;
+    }
+
+    Body.setAngle(leftFlipper, 0)
+    Body.setAngle(rightFlipper, 0)
+
+})
+
+
+
+Events.on(engine, 'beforeUpdate', function(event) {
+    for (var i = 0; i<forces.length; i++){
+        Body.applyForce(ball, ball.position, forces[i]);
+    }
+    forces=[]; // clear the array
+});
+
+
+Events.on(engine, 'collisionStart', function(event) {
+    for (var i = 0; i < event.pairs.length; i++) {
+        let bodyA = event.pairs[i].bodyA;
+        let bodyB = event.pairs[i].bodyB;
+        var resetBall = ((bodyB == ball && bodyA == oob) || (bodyA == ball && bodyB == oob));
+
+        if (resetBall) {
+          setTimeout(() => {
+            Body.setPosition(ball, {x:572, y:700})
+          }, 2000)
+        } 
+        
+        if (bodyB == ball && bodyA.label == 'bouncer') {
+            launchBall(ball, bodyA.position, .04)
+        }
+        else if (bodyA == ball && bodyB.label== 'bouncer') {
+            launchBall(ball, bodyB.position, .04)
+        }
+    
+    }
+});
+
+Events.on(engine, 'collisionActive', function(event) {
+    for (var i = 0; i < event.pairs.length; i++) {
+        let bodyA = event.pairs[i].bodyA;
+        let bodyB = event.pairs[i].bodyB;
+        if ((bodyB == ball && bodyA == spring) || (bodyA == ball && bodyB == spring)) {
+          onSpring = true;
+        }
+        
+    }
+});
+
+Runner.run(runner, engine);
+})
